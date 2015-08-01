810c0c735387f97bbe8330c8b01e6e641f02d7af
Zak Cutner <me@zakcutner.uk>
2015-08-01 18:00:29 +0100

    Made timer work

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
-var a = moment([2007, 0, 28]);
-var b = moment([2007, 0, 29]);
-console.log(a.to(b));
+function init(location) {
+    $.getJSON('http://api.open-notify.org/iss-pass.json?callback=?', {lat: location.coords.latitude, lon: location.coords.longitude, n: 1}, function(data) {
+        var eventTime = moment.unix(data.response[0].risetime);
+        var currentTime = moment()
+        var diffTime = eventTime - currentTime;
+        var duration = moment.duration(diffTime*1000, 'milliseconds');
+        var interval = 1000;
+
+        setInterval(function() {
+            duration = moment.duration(duration - interval, 'milliseconds');
+            $('body').css('display', 'table');
+            $('#timer').html('<span>' + duration.days() + '</span> : <span>' + duration.hours() + '</span> : <span>' + duration.minutes() + '</span> : <span>' + duration.seconds() + '</span>');
+        }, interval);
+    });
+}
+
+navigator.geolocation.getCurrentPosition(init);
