15ce41aac7307284826be32693f8bb1bb803bc6d
Zak Cutner <me@zakcutner.uk>
2015-08-02 21:10:53 +0100

    Timer changes

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
-function init(location) {
+navigator.geolocation.getCurrentPosition(function(location) {
     $.getJSON('http://api.open-notify.org/iss-pass.json?callback=?', {lat: location.coords.latitude, lon: location.coords.longitude}, function(data) {
-        var end = moment.unix(data.response[0].risetime);
-        var start = moment();
-        var difference = end - start;
-        var duration = moment.duration(difference * 1000, 'milliseconds');
+        var difference = moment.unix(data.response[0].risetime).diff(moment());
+        var duration = moment.duration(difference);
 
         setInterval(function() {
-            duration = moment.duration(duration - 1000, 'milliseconds');
+            duration = moment.duration(duration - 1000);
             $('body').css('display', 'table');
-            $('#timer').html('<span>' + duration.days() + '</span> : <span>' + duration.hours() + '</span> : <span>' + duration.minutes() + '</span> : <span>' + duration.seconds() + '</span>');
+            $('#timer').html('<span>' + duration.hours() + '</span> hours <span>' + duration.minutes() + '</span> minutes <span>' + duration.seconds() + '</span> seconds');
         }, 1000);
     });
-}
-
-navigator.geolocation.getCurrentPosition(init);
+});
