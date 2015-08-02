a6db05a2fefadbb1e5a894f8a3a7a200d2be08f9
Zak Cutner <me@zakcutner.uk>
2015-08-02 21:13:06 +0100

    Minor change

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 navigator.geolocation.getCurrentPosition(function(location) {
     $.getJSON('http://api.open-notify.org/iss-pass.json?callback=?', {lat: location.coords.latitude, lon: location.coords.longitude}, function(data) {
+        function repeat() {
+            duration = moment.duration(duration - 1000);
+            $('#timer').html('<span>' + duration.hours() + '</span> hours <span>' + duration.minutes() + '</span> minutes <span>' + duration.seconds() + '</span> seconds');
+        }
+
         var difference = moment.unix(data.response[0].risetime).diff(moment());
         var duration = moment.duration(difference);
 
-        setInterval(function() {
-            duration = moment.duration(duration - 1000);
-            $('body').css('display', 'table');
-            $('#timer').html('<span>' + duration.hours() + '</span> hours <span>' + duration.minutes() + '</span> minutes <span>' + duration.seconds() + '</span> seconds');
-        }, 1000);
+        repeat();
+        $('body').css('display', 'table');
+        setInterval(repeat, 1000);
     });
 });
