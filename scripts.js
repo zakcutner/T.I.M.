f95e4014189eb337f82fb45b786167f6d6dbca36
Zak Cutner <me@zakcutner.uk>
2015-08-02 09:10:21 +0100

    Changed logo

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 function init(location) {
     $.getJSON('http://api.open-notify.org/iss-pass.json?callback=?', {lat: location.coords.latitude, lon: location.coords.longitude}, function(data) {
-        var eventTime = moment.unix(data.response[0].risetime);
-        var currentTime = moment()
-        var diffTime = eventTime - currentTime;
-        var duration = moment.duration(diffTime*1000, 'milliseconds');
-        var interval = 1000;
+        var end = moment.unix(data.response[0].risetime);
+        var start = moment();
+        var difference = end - start;
+        var duration = moment.duration(difference * 1000, 'milliseconds');
 
         setInterval(function() {
-            duration = moment.duration(duration - interval, 'milliseconds');
+            duration = moment.duration(duration - 1000, 'milliseconds');
             $('body').css('display', 'table');
             $('#timer').html('<span>' + duration.days() + '</span> : <span>' + duration.hours() + '</span> : <span>' + duration.minutes() + '</span> : <span>' + duration.seconds() + '</span>');
-        }, interval);
+        }, 1000);
     });
 }
 
